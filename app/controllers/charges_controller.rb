ee4e6c8a624a8bf6ae4a71bc29bccf969f1dc9d4
Jun Liang <illuminatemynight@gmail.com>
2020-02-24 11:56:22 -0600

    charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,15 +6,15 @@ class ChargesController < ApplicationController
 
         Stripe.api_key = 'sk_test_RpZ98JKJWMCKmQk2DniXQgM300vA67J48Y'
 
-        # begin
-        #     Stripe::Charge.create({
-        #         :amount => params[:amount],
-        #         :currency => 'usd',
-        #         :source => params[:payload][:token][:id]
-        #     })
-        # rescue Stripe::CardError => e
-            render json: { message: 'oops'}
-        # end
+        begin
+            Stripe::Charge.create({
+                :amount => params[:amount],
+                :currency => 'usd',
+                :source => params[:payload][:token][:id]
+            })
+        rescue Stripe::CardError => e
+            render json: { message: 'error'}
+        end
 
     end
 end
